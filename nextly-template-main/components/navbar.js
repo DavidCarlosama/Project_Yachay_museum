b40583f8b80874d2e776bac2db6542d67c98502a
DazethDv <166768024+DavidCarlosama@users.noreply.github.com>
2024-06-12 04:34:36 -0500

    Update navbar.js

--- nextly-template-main/components/navbar.js
+++ nextly-template-main/components/navbar.js
@@ -1,12 +1,14 @@
 import Link from "next/link";
 import ThemeChanger from "./DarkSwitch";
-import Image from "next/image"
+import Image from "next/image";
 import { Disclosure } from "@headlessui/react";
 
 const Navbar = () => {
   const navigation = [
-    "Home",
-  
+    { label: "Home", url: "/" },
+    { label: "Catalogue", url: "/catalogo" },
+    { label: "Book Your visit", url: "/reservation" },
+    { label: "About", url: "/conoce" },  
   ];
 
   return (
@@ -18,23 +20,23 @@ const Navbar = () => {
             <>
               <div className="flex flex-wrap items-center justify-between w-full lg:w-auto">
                 <Link href="/">
-                  <span className="flex items-center space-x-2 text-2xl font-medium text-indigo-500 dark:text-gray-100">
+                  <span className="flex items-center space-x-2 text-2xl font-medium text-black-500 dark:text-black-100">
                     <span>
                       <Image
-                        src="/img/logo.png"
+                        src="/img/logo.svg"
                         alt="N"
                         width="32"
                         height="32"
-                        className="w-8"
+                        className="w-11"
                       />
                     </span>
-                    <span>Yachay Tech</span>
+                    <span>Yachay Tech Museum</span>
                   </span>
                 </Link>
 
                 <Disclosure.Button
                   aria-label="Toggle Menu"
-                  className="px-2 py-1 ml-auto text-gray-500 rounded-md lg:hidden hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:text-gray-300 dark:focus:bg-trueGray-700">
+                  className="px-2 py-1 ml-auto text-black-500 rounded-md lg:hidden hover:text-black-500 focus:text-black-500 focus:bg-black-100 focus:outline-none dark:text-black-300 dark:focus:bg-trueGray-700">
                   <svg
                     className="w-6 h-6 fill-current"
                     xmlns="http://www.w3.org/2000/svg"
@@ -56,16 +58,24 @@ const Navbar = () => {
                 </Disclosure.Button>
 
                 <Disclosure.Panel className="flex flex-wrap w-full my-5 lg:hidden">
-                  <>
+                  <ul className="w-full">
                     {navigation.map((item, index) => (
-                      <Link key={index} href="/" className="w-full px-4 py-2 -ml-4 text-gray-500 rounded-md dark:text-gray-300 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 dark:focus:bg-gray-800 focus:outline-none">
-                          {item}
-                      </Link>
+                      <li key={index} className="w-full px-4 py-2 -ml-4">
+                        <Link href={item.url}>
+                          <span className="text-black-500 rounded-md dark:text-black-300 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 dark:focus:bg-gray-800 focus:outline-none">
+                            {item.label}
+                          </span>
+                        </Link>
+                      </li>
                     ))}
-                    <Link href="/" className="w-full px-6 py-2 mt-3 text-center text-white bg-indigo-200 rounded-md lg:ml-5">         
-                        Get Started
-                    </Link>
-                  </>
+                    <li className="w-full px-6 py-2 mt-3 text-center">
+                      <Link href="/">
+                        <span className="text-white bg-black-200 rounded-md lg:ml-5">
+                          Get Started
+                        </span>
+                      </Link>
+                    </li>
+                  </ul>
                 </Disclosure.Panel>
               </div>
             </>
@@ -77,38 +87,23 @@ const Navbar = () => {
           <ul className="items-center justify-end flex-1 pt-6 list-none lg:pt-0 lg:flex">
             {navigation.map((menu, index) => (
               <li className="mr-3 nav__item" key={index}>
-              <Link href="/" className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-gray-800">
-                {menu}
-              </Link>
-            </li>
-          ))}
-
-            <li className="mr-3 nav__item">
-            <Link href="/catalogo" className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-gray-800">
-              Catalogue
-              </Link>
-            </li>
-
-             <li className="mr-3 nav__item">
-            <Link href="/reservation" className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-gray-800">
-              Book your visit
-            </Link>
-            </li>
-
-            <li className="mr-3 nav__item">
-            <Link href="/conoce" className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-gray-800">
-              About
-            </Link>
-            </li>
-
+                <Link href={menu.url}>
+                  <span className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-gray-800">
+                    {menu.label}
+                  </span>
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
 
         <div className="hidden mr-3 space-x-4 lg:flex nav__item">
-          <Link href="/" className="px-6 py-2 text-white bg-indigo-600 rounded-md md:ml-5">
-              Virtual Visit
+          <Link href="/">
+            <span className="px-6 py-2 text-white bg-indigo-600 rounded-md md:ml-5">
+              Virtual Visit 
+              (comings soon)
+            </span>
           </Link>
-
           <ThemeChanger />
         </div>
       </nav>
@@ -116,4 +111,4 @@ const Navbar = () => {
   );
 }
 
-export default Navbar;
+export default Navbar;
