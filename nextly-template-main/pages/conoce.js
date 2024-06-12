82bb4ea0dc1668d8ea4018d1ca152bcc9c951fd6
DazethDv <166768024+DavidCarlosama@users.noreply.github.com>
2024-06-12 04:48:50 -0500

    Update conoce.js

--- nextly-template-main/pages/conoce.js
+++ nextly-template-main/pages/conoce.js
@@ -3,12 +3,11 @@ import Head from "next/head";
 import Navbar from "../components/navbar";
 import SectionTitle from "../components/AbouTitle";
 
+import Head from "next/head";
+import Navbar from "../components/navbar";
 import { benefitOne, benefitTwo } from "../components/About_data";
-import Video from "../components/video";
 import Benefits from "../components/benefits";
 import Footer from "../components/footer";
-import Testimonials from "../components/testimonials";
-import Faq from "../components/faq";
 import PopupWidget from "../components/popupWidget";
 import About from "../components/About";
 
@@ -16,11 +15,7 @@ const History = () => {
   return (
     <>
       <Head>
-        <title>Yachay Archaeological Museum</title>
-        <meta
-          name="description"
-          content="Nextly is a free landing page template built with next.js & Tailwind CSS"
-        />
+      <title>Yachay Archaeological Museum</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
